550792a70b60f6064baf098deb18f9d78933f44b
Tomasz Lipka <lipkatk@gmail.com>
2024-01-15 14:37:11 +0100

    add church data initializer

--- src/main/java/com/accenture/academy/church/ChurchDataInitializer.java
+++ src/main/java/com/accenture/academy/church/ChurchDataInitializer.java
@@ -20,5 +20,23 @@ class ChurchDataInitializer {
         20000.0
         );
         churchService.addChurch(churchDao);
+
+        ChurchDao churchDao2 = new ChurchDao(
+                "Kościół sw. Krzyża",
+                "Łódź",
+                130,
+                1000,
+                20000.0
+        );
+        churchService.addChurch(churchDao2);
+
+        ChurchDao churchDao3 = new ChurchDao(
+                "Kościół sw. Krzyża",
+                "Kraków",
+                130,
+                1000,
+                20000.0
+        );
+        churchService.addChurch(churchDao3);
     }
 }
