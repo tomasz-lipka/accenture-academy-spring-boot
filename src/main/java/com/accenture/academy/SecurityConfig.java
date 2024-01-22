765d3ccc0712cd3aed39a01f977dbcd7ec4ed6ab
Tomasz Lipka <lipkatk@gmail.com>
2024-01-22 15:03:33 +0100

    update security

--- src/main/java/com/accenture/academy/SecurityConfig.java
+++ src/main/java/com/accenture/academy/SecurityConfig.java
@@ -3,9 +3,19 @@ package com.accenture.academy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 public class SecurityConfig {
@@ -14,10 +24,33 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(
                 request -> request
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET,"/api")).hasRole("USER")
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST,"/api")).hasRole("ADMIN")
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**")).hasRole("USER")
+//                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST,"/api/**")).hasRole("ADMIN")
                         .anyRequest()
                         .permitAll()
-        ).build();
+        )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService getUser(){
+        UserDetails admin = User.builder()
+                .username("Tomek")
+                .password(encoder().encode("123"))
+                .roles("ADMIN", "USER")
+                .build();
+        UserDetails user = User.builder()
+                .username("Leo")
+                .password(encoder().encode("123"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
