bd97503724508b7c431c44e0c6d3918b8a1f5354
Tomasz Lipka <lipkatk@gmail.com>
2024-01-23 12:22:31 +0100

    add new logic and tests

--- src/main/java/com/accenture/academy/calculator/Calculator.java
+++ src/main/java/com/accenture/academy/calculator/Calculator.java
@@ -2,18 +2,30 @@ package com.accenture.academy.calculator;
 
 public class Calculator {
 
-    public Integer add(int x, int y){
+    public Integer add(int x, int y) throws IllegalArgumentException {
+        if (x < 0 || y < 0 || x > 100 || y > 100){
+            throw new IllegalArgumentException();
+        }
         return x + y;
     }
-    public Integer substract(int x, int y){
+
+    public Integer substract(int x, int y) throws IllegalArgumentException {
+        if (x < 0 || y < 0 || x > 100 || y > 100){
+            throw new IllegalArgumentException();
+        }
         return x - y;
     }
-    public Integer multiply(int x, int y){
+
+    public Integer multiply(int x, int y) throws IllegalArgumentException {
+        if (x < 1 || y < 1 || x > 30 || y > 30){
+            throw new IllegalArgumentException();
+        }
         return x * y;
     }
+
     public Integer divide(int x, int y) throws IllegalArgumentException {
-        if(y == 0){
-            throw new IllegalArgumentException("Can't divide by zero");
+        if (x < 0 || y < 1 || x > 1000 || y > 100){
+            throw new IllegalArgumentException();
         }
         return x / y;
     }
--- src/test/java/com/accenture/academy/calculator/CalculatorTest.java
+++ src/test/java/com/accenture/academy/calculator/CalculatorTest.java
@@ -51,4 +51,56 @@ class CalculatorTest {
     void whenDivideByZeroThenReturnException(){
         Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(2,0));
     }
+
+    @Test
+    void whenAddInputsAreBelow0ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.add(-1,7));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.add(7,-1));
+    }
+    @Test
+    void whenAddInputsAreAbove100ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.add(101,7));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.add(7,101));
+    }
+    @Test
+    void whenSubtractInputsAreBelow0ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.substract(-1,7));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.substract(7,-1));
+    }
+    @Test
+    void whenSubtractInputsAreAbove100ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.substract(101,7));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.substract(7,101));
+    }
+
+    @Test
+    void whenMultiplyInputsAreBelow1ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.multiply(0,7));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.multiply(7,0));
+    }
+
+    @Test
+    void whenMultiplyInputsAreAbove30ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.multiply(31,7));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.multiply(7,31));
+    }
+
+    @Test
+    void whenDivideFirstInputIsBelow0ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(-1,7));
+    }
+
+    @Test
+    void whenDivideSecondInputIsBelow1ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(7,0));
+    }
+
+    @Test
+    void whenDivideFirstInputIsAbove1000ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(1001,7));
+    }
+    @Test
+    void whenDivideSecondInputIsAbove100ThenReturnException(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(7,101));
+    }
 }
