d19a4f69e5fde9a2449991e0cfff93ed2f972ae5
Tomasz Lipka <lipkatk@gmail.com>
2024-01-23 14:45:21 +0100

    add mocked ChurchService crud methods

--- src/test/java/com/accenture/academy/church/ChurchServiceTest.java
+++ src/test/java/com/accenture/academy/church/ChurchServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,5 +32,58 @@ class ChurchServiceTest {
         );
         Mockito.when(churchRepository.findAll()).thenReturn(List.of(churchDao));
         assertEquals(List.of(churchDao), churchService.getAllChurches());
+    }
+
+    @Test
+    void testAddChurch(){
+        ChurchDao churchDao = new ChurchDao(
+                "Kościół sw. Krzyża",
+                "Warszawa",
+                130,
+                1000,
+                20000.0
+        );
+        churchService.addChurch(churchDao);
+        Mockito.verify(churchRepository, Mockito.times(1)).save(churchDao);
+    }
+
+    @Test
+    void testGetById(){
+        ChurchDao churchDao = new ChurchDao(
+                "Kościół sw. Krzyża",
+                "Warszawa",
+                130,
+                1000,
+                20000.0
+        );
+        Mockito.when(churchRepository.findByID(1L)).thenReturn(Optional.of(churchDao));
+        assertEquals(churchDao, churchService.getChurchById(1L));
+    }
+
+    @Test
+    void testUpdateChurch() {
+        ChurchDto churchDto = new ChurchDto(
+                "Kościół sw. Krzyża",
+                "Warszawa",
+                130,
+                1000,
+                20000.0
+        );
+        ChurchDao churchDao = new ChurchDao(
+                "Kościół sw. Krzyża",
+                "Warszawa",
+                130,
+                1000,
+                20000.0
+        );
+        churchDao.setId(1L);
+        churchService.updateChurch(1L, churchDto);
+        Mockito.verify(churchRepository, Mockito.times(1)).save(churchDao);
+    }
+
+    @Test
+    void testDeleteChurchById(){
+        churchService.deleteChurchById(1L);
+        Mockito.verify(churchRepository, Mockito.times(1)).deleteById(1L);
     }
 }
