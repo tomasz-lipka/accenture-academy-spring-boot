97a1b0cd1f7622508e9d15388c821e7e0bfe2dd1
Tomasz Lipka <lipkatk@gmail.com>
2024-01-23 11:24:20 +0100

    add tests for all methods

--- src/test/java/com/accenture/academy/calculator/CalculatorTest.java
+++ src/test/java/com/accenture/academy/calculator/CalculatorTest.java
@@ -3,8 +3,6 @@ package com.accenture.academy.calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CalculatorTest {
 
     @Test
@@ -15,5 +13,35 @@ class CalculatorTest {
         Integer result = calculator.add(1, 4);
         //Then
         Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    void testSubtract(){
+        //Given
+        Calculator calculator = new Calculator();
+        //When
+        Integer result = calculator.substract(1, 4);
+        //Then
+        Assertions.assertEquals(-3, result);
+    }
+
+    @Test
+    void testMultiply(){
+        //Given
+        Calculator calculator = new Calculator();
+        //When
+        Integer result = calculator.multiply(1, 4);
+        //Then
+        Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    void testDivide(){
+        //Given
+        Calculator calculator = new Calculator();
+        //When
+        Integer result = calculator.divide(8, 2);
+        //Then
+        Assertions.assertEquals(4, result);
     }
 }
